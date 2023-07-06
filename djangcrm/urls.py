3ebcfb0a07529fedd8142633ebb01c8b2da71499
Cristiano Angeli <cristiano.a992@gmail.com>
2023-07-06 15:31:53 +0200

    reset password

--- djangcrm/urls.py
+++ djangcrm/urls.py
@@ -3,7 +3,14 @@ from django.urls import path, include
 from leads.views import LandingPage, SignupView
 from django.conf.urls.static import static
 from django.conf import settings
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView, 
+    LogoutView, 
+    PasswordResetView, 
+    PasswordResetDoneView, 
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+    )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,6 +20,10 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('signup/', SignupView.as_view(), name='signup'),
+    path('reset-password/', PasswordResetView.as_view(), name="reset-password"),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('password-reser-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
 
 if settings.DEBUG:
